c5d63e8df7951766f359a55b5fb96c129d1267b7
Johannes Wagener <johannes@soundcloud.com>
2011-01-16 23:40:04 -0800

    comments and git ignorance

--- lib/httmultiparty/special_hash.rb
+++ lib/httmultiparty/special_hash.rb
@@ -1,5 +1,7 @@
+# SpecialHash is a hash that is pretending to be not a Hash.
+# its just a cheap way to get around the query normalization
+# happening in HTTParty::Request#body in lib/httparty/request.rb:118
 class HTTMultiParty::SpecialHash < Hash
-  # pretending not to be a hash
   def is_a?(klass)
     klass == ::Hash ? false : super
   end
