97da08e9c29223bc91973913b0d2fc9fa879f75d
Ryan Bigg <radarlistener@gmail.com>
2014-03-21 15:01:18 +1100

    Add regression spec for #11

---
+++ spec/httmultiparty/request_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+class Client
+  include HTTMultiParty
+  base_uri 'http://example.com'
+  default_params({ token: 'test' })
+end
+
+describe HTTMultiParty do
+  let(:somefile) { File.new(File.join(File.dirname(__FILE__), '../fixtures/somefile.txt')) }
+
+  it "makes the request with correct query parameters" do
+    FakeWeb.register_uri(:post, "http://example.com/foobar?token=test", :body => "hello world")
+    Client.post(
+      '/foobar',
+      body: {
+        attachment: somefile
+      }
+    )
+  end
+end
+
